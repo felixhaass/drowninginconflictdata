b87194406e8884bc5fa38e5eb19efcf1bebf7e70
Felix Haass <haass.felix@gmail.com>
2015-03-27 18:39:18 +0100

    small script fixes

--- scrape_plot_datafeatures.R
+++ scrape_plot_datafeatures.R
@@ -1,13 +1,19 @@
+# Script for Blog Post "Are We Drowning in Conflict Data?"
+# Author: Felix Haass
+# Licence: CC BY-SA-NC
+# 
+# The script is using a modified version of the 
+
 library(ggplot2)
+library(Cairo)
 
-source("")
-
+source("GScholar_scrape.R")
 
 #######
 # JPR #
 #######
 
-jpr <- GScholar_Scraper("allintitle: data OR dataset", journal = "Journal+of+Peace+Research")
+jpr <- GScholar_Scraper("allintitle: data OR dataset", journal = "\"Journal+of+Peace+Research\"")
 jpr$pub <- "JPR"
 jpr$count <- 1
 
@@ -21,7 +27,7 @@ jpr <- rbind(jpr, data.frame(TITLES = NA,
 # International Interactions #
 ##############################
 
-ii <- GScholar_Scraper("allintitle: data OR dataset", journal = "International+Interactions")
+ii <- GScholar_Scraper("allintitle: data OR dataset", journal = "\"International+Interactions\"")
 ii$pub <- "II"
 ii$count <- 1
 
@@ -35,7 +41,7 @@ ii <- rbind(ii, data.frame(TITLES = NA,
 # CMPS #
 ########
 
-cmps <- GScholar_Scraper("allintitle: data OR dataset", journal = "Conflict+Management+and+Peace+Science")
+cmps <- GScholar_Scraper("allintitle: data OR dataset", journal = "\"Conflict+Management+and+Peace+Science\"")
 cmps$pub <- "CMPS"
 cmps$count <- 1
 
@@ -45,12 +51,29 @@ cmps <- rbind(cmps, data.frame(TITLES = NA,
                                pub = "CMPS", count = 0))
 
 # combine & aggregate datasets
+jpr <- jpr %>% mutate(issues = ifelse(YEAR < 1998, 4, 6))
+
+# manual coding of cmps issues / year
+cmps_issues <- data.frame(YEAR = 1990:2014, 
+                          issues = c(1, 1, 1, 2, 1, 2,2,0,2,2,1,1,2,2,4,4,4,4,4,5,5,5,5,5,5))
+
+cmps <- merge(cmps, cmps_issues, by = "YEAR", all.x = TRUE)
+
+# international interactions issues / year
+ii <- ii %>% mutate(issues = ifelse(YEAR < 2011, 4, 5))
+
 data_articles <- rbind(jpr, ii, cmps)
-df_ag <- aggregate(count ~ YEAR + pub, data_articles, sum)
-
-# without 2015
-df_ag <- df_ag[df_ag$YEAR != 2015, ]
-
+df_ag <- data_articles %>% 
+  filter(YEAR != 2015) %>% 
+  group_by(YEAR, pub) %>% 
+  summarize(count = sum(count),
+            issues = min(issues),
+            count_by_issue = count / issues) %>% 
+  ungroup() %>% 
+  arrange(pub, YEAR)
+ 
+# Plots!
+ 
 plot_df <- ggplot(df_ag, aes(x=YEAR, y = count, fill = pub)) +
   geom_area(aes(fill = pub, group = pub), position = "stack") +
   geom_line(position = "stack", aes(ymax = 26)) + 
@@ -59,21 +82,37 @@ plot_df <- ggplot(df_ag, aes(x=YEAR, y = count, fill = pub)) +
   theme_bw() +
   scale_x_continuous(breaks = seq(1990,2014,2)) +
   scale_y_continuous(breaks = seq(0, 25, 5)) +
-  labs(x = "", y = "No. of Data Articles / year")
+  labs(x = "", y = "No. of Data Articles ", title = "Search for \"data OR dataset\" in article title\n")
 
-
+CairoPNG(file = "data_trend.png", width = 9.39, height = 4.41, units = "in", dpi = 300)
 plot_df
+dev.off()
 
-###
-# "new data
-#
+# plot counts by issue
+
+plot_df_issues <- ggplot(df_ag, aes(x=YEAR, y = count_by_issue, fill = pub)) +
+  geom_area(aes(fill = pub, group = pub), position = "stack") +
+  geom_line(position = "stack", aes(ymax = 26)) + 
+  geom_point(position = "stack", type = 5, size = 1.2, aes(ymax = 26)) +
+  scale_fill_brewer(name = "Journal", palette = "Set1") + 
+  theme_bw() +
+  scale_x_continuous(breaks = seq(1990,2014,2)) +
+  labs(x = "", y = "No. of Data Articles / issues", title = "Search for \"data OR dataset\" in article title\n")
+
+CairoPNG(file = "data_trend_issue.png", width = 9.39, height = 4.41, units = "in", dpi = 300)
+plot_df_issues
+dev.off()
+
+############################################
+# Search for 'new data' in article + title #
+############################################
 
 
 #######
 # JPR #
 #######
 
-jpr <- GScholar_Scraper("\"new data\"", journal = "Journal+of+Peace+Research")
+jpr <- GScholar_Scraper("\"new data\"", journal = "\"Journal+of+Peace+Research\"")
 jpr$pub <- "JPR"
 jpr$count <- 1
 
@@ -87,7 +126,7 @@ jpr <- rbind(jpr, data.frame(TITLES = NA,
 # International Interactions #
 ##############################
 
-ii <- GScholar_Scraper("\"new data\"", journal = "International+Interactions")
+ii <- GScholar_Scraper("\"new data\"", journal = "\"International+Interactions\"")
 ii$pub <- "II"
 ii$count <- 1
 
@@ -101,7 +140,7 @@ ii <- rbind(ii, data.frame(TITLES = NA,
 # CMPS #
 ########
 
-cmps <- GScholar_Scraper("\"new data\"", journal = "Conflict+Management+and+Peace+Science")
+cmps <- GScholar_Scraper("\"new data\"", journal = "\"Conflict+Management+and+Peace+Science\"")
 cmps$pub <- "CMPS"
 cmps$count <- 1
 
@@ -110,21 +149,43 @@ cmps <- rbind(cmps, data.frame(TITLES = NA,
                                YEAR = c(1990:2014)[!(1990:2014 %in% cmps$YEAR )], 
                                pub = "CMPS", count = 0))
 
+# add issues
+jpr <- jpr %>% mutate(issues = ifelse(YEAR < 1998, 4, 6))
+
+# manual coding of cmps issues / year
+cmps_issues <- data.frame(YEAR = 1990:2014, 
+                          issues = c(1, 1, 1, 2, 1, 2,2,0,2,2,1,1,2,2,4,4,4,4,4,5,5,5,5,5,5))
+
+cmps <- merge(cmps, cmps_issues, by = "YEAR", all.x = TRUE)
+
+# international interactions issues / year
+ii <- ii %>% mutate(issues = ifelse(YEAR < 2011, 4, 5))
+
 # combine & aggregate datasets
 data_articles <- rbind(jpr, ii, cmps)
-df_ag <- aggregate(count ~ YEAR + pub, data_articles, sum)
 
-# without 2015
-df_ag <- df_ag[df_ag$YEAR != 2015, ]
 
-plot_df <- ggplot(df_ag, aes(x=YEAR, y = count, fill = pub)) +
+df_ag2 <- data_articles %>% 
+  filter(YEAR != 2015) %>% 
+  group_by(YEAR, pub) %>% 
+  summarize(count = sum(count),
+            issues = min(issues),
+            count_by_issue = count / issues) %>% 
+  ungroup() %>% 
+  group_by(pub) %>% 
+  mutate(cumsum_count = cumsum(count)) %>% 
+  arrange(pub, YEAR)
+
+plot_all_data <- ggplot(df_ag2, aes(x=YEAR, y = count, fill = pub)) +
   geom_area(aes(fill = pub, group = pub), position = "stack") +
   geom_line(position = "stack", aes(ymax = 26)) + 
   geom_point(position = "stack", type = 5, size = 1.2, aes(ymax = 26)) +
   scale_fill_brewer(name = "Journal", palette = "Set1") + 
   theme_bw() +
   scale_x_continuous(breaks = seq(1990,2014,2)) +
-  scale_y_continuous(breaks = seq(0, 60, 5)) +
-  labs(x = "", y = "No. of Data Articles / year")
+  labs(x = "", y = "No. of Data Articles", title = "Search for \"new data\" in article title & body \n" )
 
-plot_df
+CairoPNG(file = "data_trend_all.png", width = 9.39, height = 4.41, units = "in", dpi = 300)
+plot_all_data
+dev.off()
+
